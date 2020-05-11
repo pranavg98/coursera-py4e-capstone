5fa9b629be7bda85ac3c3b2125514d5feb848e9d
pranavg98 <65190071+pranavg98@users.noreply.github.com>
2020-05-12 00:24:44 +0530

    Add files via upload

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','indiana.edu','ucdavis.edu','berkeley.edu','mac.com','uct.ac.za','gmail.com','cam.ac.uk','yale.edu','virginia.edu'],
+['2005',57,12,11,12,12,14,10,7,5,9],
+['2006',254,68,65,64,54,51,52,51,53,44]
+];
